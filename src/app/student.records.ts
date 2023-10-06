6a9273feb9c35cdecf8ff3fdeb2a4afd4b857481
P4ul0Silva <paulors43@outlook.com>
2023-10-06 17:42:55 -0300

    Data Binding + Event Binding Basics!

---
+++ src/app/student.records.ts
@@ -0,0 +1,9 @@
+// mock de estudantes
+
+import { Student } from "./student";
+
+export const STUDENTS: Student[] = [
+    {id: 101, name: "Edgar", age: 55},
+    {id: 102, name: "George", age: 28},
+    {id: 103, name: "Bob", age: 36}
+];
---
+++ src/app/student.ts
@@ -0,0 +1,13 @@
+// criação do tipo Student
+
+export class Student {
+    id: number;
+    name: string;
+    age: number;
+
+    constructor(id: number, name: string, age: number) {
+        this.id = id,
+        this.name = name,
+        this.age = age
+    }
+}
